545f317d3e43565aec6cab27a5a835215a9a5e66
Jailson Junior <jailson.morais-ext@treves-group.com>
2023-06-21 13:24:29 -0300

    Feat: Created Grid to list devices

--- src/app/devices/page.tsx
+++ src/app/devices/page.tsx
@@ -19,34 +19,277 @@ export default function Devices() {
           <nav className={styles.nav}>
             <Link href='/dash' className={styles.navlink}>
               <span className={`material-symbols-outlined ${styles.navicon}`}>grid_view</span>
-              <span className={styles.navtext}>Dash</span>
+              <span className={styles.navtext}>Dashboard</span>
             </Link>
-            <Link href='/people' className={styles.navlink}>
+            <Link href='/users' className={styles.navlink}>
               <span className={`material-symbols-outlined ${styles.navicon}`}>person</span>
-              <span className={styles.navtext}>Pessoas</span>
+              <span className={styles.navtext}>Users</span>
             </Link>
             <Link href='/devices' className={styles.navlink}>
               <span className={`material-symbols-outlined ${styles.navicon}`}>computer</span>
-              <span className={styles.navtext}>Equipamentos</span>
+              <span className={styles.navtext}>Devices</span>
             </Link>
             <Link href='/contract' className={styles.navlink}>
               <span className={`material-symbols-outlined ${styles.navicon}`}>contract</span>
-              <span className={styles.navtext}>Contratos</span>
+              <span className={styles.navtext}>Contracts</span>
             </Link>
           </nav>
       </div>
       </div>
       <div className={styles.submenu}>
-        <div className={styles.filter}>
-          <div className={styles.title}>Equipamentos</div>
-          <input type='text' placeholder='pesquise por id, serial' className={styles.field}/>
-        </div>
-        <span className={styles.total}>150 equipamentos</span>
+        <div className={styles.title}>Devices</div>
+        <input type='text' placeholder='Search for ID, Model, Brand' className={styles.field}/>
+        <span className={styles.total}>150 devices</span>
       </div>
       <div className={styles.grid}>
-          <div className={styles.gridheader}>header</div>
-          <div className={styles.gridcontent}>content</div>
-          <div className={styles.gridfooter}>footer</div>
+        <table border-collapse="collapse">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Plant</th>
+              <th>Brand</th>
+              <th>Model</th>
+              <th>Serial</th>
+              <th>User</th>
+              <th>Delivered</th>
+              <th>Owner</th>
+              <th>End Loan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+            <tr>
+              <td>DT-BR87-003</td>
+              <td>Quatro Barras</td>
+              <td>Dell</td>
+              <td>Latitude 5400</td>
+              <td>AKJF44985</td>
+              <td>Jailson Junior</td>
+              <td>12/04/2023</td>
+              <td>04958 - Telefonica</td>
+              <td>30/05/2024</td>
+            </tr>
+
+          </tbody>
+          <tfoot>
+            <span>Anterior</span>
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+            <span>6</span>
+            <span>Proximo</span>
+          </tfoot>
+        </table>
       </div>
     </main>
   )
