2179a30c1fbba0a790840698c5ca8776af9a42be
Jailson Jr <jailson94junior@gmail.com>
2024-04-30 22:05:49 -0300

    Fix: remove brands reference

--- app/api/brands/route.ts
+++
@@ -1,9 +0,0 @@
-import {  NextResponse, NextRequest } from "next/server";
-
-import { getBrands } from '../../../data/brands';
-
-export async function GET() {
-  const result = await getBrands();
-  return NextResponse.json(result);
-}
-
