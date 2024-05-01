57a8168b1214e5e3ba8496478844e96b0292bf18
Jailson Jr <jailson94junior@gmail.com>
2024-05-01 20:57:40 -0300

    Add speed analyzer

--- components/MainLayout/mainLayout.tsx
+++ components/MainLayout/mainLayout.tsx
@@ -3,11 +3,14 @@ import styles from './mainLayout.module.css';
 import MainMenu from '@/components/mainNavigation/mainNavigation';
 import AppBar from '@/components/AppBar/appBar';
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function MainLayout(props: any) {
   
   return (    
       <main className={styles.main}>
+        <SpeedInsights />
           <AppBar />
           <section className={styles.twoColumns}>
             {props.limited_view ? '' : <MainMenu />}
